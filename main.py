8c6ef0981c8517457c649485d19ef6a65dc06df9
Xebec-coder <adelartey135@gmail.com>
2025-09-29 10:36:36 +0300

    Work-02

--- main.py
+++ main.py
@@ -32,5 +32,39 @@ client = Cart('John', my_bag)
 client.add("bag")
 client.add("key")
 client.add("sword")
-# client.delete("bag")
+client.delete("bag")
 client.show()
+
+
+# Завдання 2
+# Створіть клас Phone з атрибутами number та battery_level.
+# Додайте метод який зменшує заряд телефона(на скільки
+# зменшити відсотків передається як параметр), якщо він
+# опуститься нижче 20%, вивести повідомлення
+# Додайте метод для виведення інформації про телефон
+
+class Phone:
+
+    def __init__(self, number, battery_level=100):
+        self.number = number
+        self.battery_level = battery_level
+
+
+    def used(self, percent):
+        self.battery_level -= percent
+
+        if  self.battery_level <= 20:
+            print("Увага у вас залишилось меньше 20% заряду батареї")
+
+        print(f"Залишилось {self.battery_level}% заряду батареї")
+
+
+    def info(self):
+        print(f"Номер телефону -- {self.number}"
+              f"\nЗаряд телефону -- {self.battery_level}%")
+
+telephone = Phone(32445234)
+
+telephone.info()
+telephone.used(20)
+telephone.used(60)
